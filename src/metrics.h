32da498a2e55fa485ff8e71d269c411b464c0ed3
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-01-31 11:32:11 +0300

    Update metrics.h

--- src/metrics.h
+++ src/metrics.h
@@ -5,32 +5,30 @@
 
 std::mutex console_m;
 
-
 class metric {
 public:
-	int _str_ch=0;
-	int _cmd_ch=0;
-	int _block_ch=0;
+	std::size_t _str_ch=0;
+	std::size_t _cmd_ch=0;
+	std::size_t _block_ch=0;
 	std::string _name;
 	const bool _is_main;
 	
 	metric( const std::string name, bool is_main = false):_is_main(is_main), _name(name) {}
 
 	~metric() {
-		
+		console_m.lock();
 		std::cout << _name << " thread - ";
 		if (_is_main) { 
-			console_m.lock();
+			
 			std::cout << _str_ch << " string, " << _cmd_ch << " command, " <<
 			_block_ch << " blok." << std::endl; 
-			console_m.unlock();
+			
 		}
 		else {
-			console_m.lock();
+			
 			std::cout << _block_ch << " blok, " << _cmd_ch << " command." << std::endl;
-			console_m.unlock();
+			
 		}
-		
-		
+		console_m.unlock();
 	}
 };
