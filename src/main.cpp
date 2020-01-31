678d92f60506daa6bc12e26f93797fc2ae874dc4
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-01-31 11:26:37 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,14 @@
 #include "command.h"
 
 int main(int, char* argv[]) {
-
-
-	//Command command_log(std::atoi(argv[1]));
-
-	metric m_main{ "main", true };
-	//auto m_log = new metric("log");
-	//auto m_file1 = new metric("File1");
-	//auto m_file2 = new metric("File2");
-
-	Command command_log(3, m_main);
-	command_log.subscribe(std::make_unique<FileObserver>(2));
-	//command_log.subscribe(std::make_unique<FileObserver>());
-	command_log.subscribe(std::make_unique<TerminalObserver>());
-	command_log.add_command();
 	
-	//command_log.~Command();
-
-	//m_main.~metric();
-	//delete m_log
-	//delete m_file1;
-	//delete m_file2;
-
-	return 0;
+	auto m_file2 = std::make_shared<metric>("File2");
+	auto m_file1 = std::make_shared<metric>("File1");
+	auto m_log = std::make_shared<metric>("log");
+	auto m_main = std::make_shared<metric>( "main", true);
+	
+	Command command_log(std::atoi(argv[1]), m_main);
+	command_log.subscribe(std::make_unique<FileObserver>(m_file1, m_file2));
+	command_log.subscribe(std::make_unique<TerminalObserver>(m_log));
+	command_log.add_command();
 }
