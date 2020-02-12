e5d6f465aac3b6d730224df5fa8ff80aec463b65
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-12 11:39:53 +0300

    Update command.h

--- src/command.h
+++ src/command.h
@@ -13,22 +13,35 @@
 #include <sstream>
 #include "observer.h"
 
+
 using observers = std::vector<std::unique_ptr<Observers>>;
 /**
 * @brief Основной класс, реализующий функционал считывания команд из стандартного ввода и распределения на блоки команд
 *
 */
 class Command {
-	
 	observers _obs;
 	commands _comm;
+
 	int _block_size;
 	int _comm_counter = 0;
+	int _bracket_counter = 0;
+	bool _is_reg = true;
 	std::string _time;
-	std::shared_ptr<metric> _m_main;
-	
+	std::shared_ptr<Metric> _m_main = std::make_shared<Metric>("main", true);
+		
 public:
-	Command(int N, std::shared_ptr<metric> m_main):_block_size(N), _m_main(m_main){}
+	Command(int N):_block_size(N){
+		v_m.emplace_back(_m_main);
+	}
+	
+	~Command() {
+		if (_is_reg) {
+			if (_comm_counter) {
+				notify();
+			}
+		}
+	}
 
 	void subscribe(std::unique_ptr<Observers>&& obs) {
 		_obs.emplace_back(std::move(obs));
@@ -49,49 +62,31 @@ public:
 		_time.clear();
 	}
 
-	void add_command_bracket() {
-		int bracket_counter = 1;
-		std::string s;
-		while (std::getline(std::cin, s)) {
-			if (std::cin.eof()) { return; }
-			_m_main->_str_ch += 1;
-			if (s[0] == '{') {
-				++bracket_counter;
-				continue;
+	void add_command(const std::string& s) {
+		if (std::cin.eof()) { return; }
+		_m_main->_str_ch += 1;
+		if (s[0] == '{') {
+			if (_comm_counter) {
+				notify();
+				_comm_counter = 0;
 			}
 
-			else if (s[0] == '}') {
-				--bracket_counter;
-				if (!bracket_counter) {
-					notify();
-					return;
-				}
-			}
-			
-			else {
-				if (_comm.empty()) { setTime(); }
-				_comm.push_back(s);
+			++_bracket_counter;
+			_is_reg = false;
+			return;
+		}
+		else if (s[0] == '}') {
+			--_bracket_counter;
+			if (!_bracket_counter) {
+				notify();
+				_is_reg = true;
 			}
 		}
-	}
 
-	void add_command() {
-		std::string s;
-		while (std::getline(std::cin, s)) {
-			if (std::cin.eof()) {
-				break;
-			}
-			_m_main->_str_ch += 1;
-			if (s[0] == '{') {
-				if (_comm_counter) {
-					notify();
-					_comm_counter = 0;
-				}
-				add_command_bracket();
-			}
-			else {
-				if (_comm.empty()) { setTime(); }
-				_comm.push_back(s);
+		else {
+			if (_comm.empty()) { setTime(); }
+			_comm.push_back(s);
+			if (_is_reg) {
 				++_comm_counter;
 				if (_comm_counter == _block_size) {
 					notify();
@@ -99,6 +94,6 @@ public:
 				}
 			}
 		}
-		if (_comm_counter) { notify(); }
 	}
+
 };
