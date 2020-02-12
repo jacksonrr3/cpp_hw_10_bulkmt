f80c24c5f0ff32d316bb0ae4dd24f54473108462
jacksonrr3 <49640784+jacksonrr3@users.noreply.github.com>
2020-02-12 11:39:49 +0300

    Update metrics.h

--- src/metrics.h
+++ src/metrics.h
@@ -2,10 +2,12 @@
 #include <string>
 #include <iostream>
 #include <mutex>
+#include <vector>
+#include <memory>
 
 std::mutex console_m;
 
-class metric {
+class Metric {
 public:
 	std::size_t _str_ch=0;
 	std::size_t _cmd_ch=0;
@@ -13,22 +15,21 @@ public:
 	std::string _name;
 	const bool _is_main;
 	
-	metric( const std::string name, bool is_main = false): _name(name), _is_main(is_main) {}
+	Metric( const std::string name, bool is_main = false): _name(name), _is_main(is_main) {}
 
-	~metric() {
+	~Metric() {
 		console_m.lock();
 		std::cout << _name << " thread - ";
 		if (_is_main) { 
-			
 			std::cout << _str_ch << " string, " << _cmd_ch << " command, " <<
 			_block_ch << " blok." << std::endl; 
-			
 		}
 		else {
 			
 			std::cout << _block_ch << " blok, " << _cmd_ch << " command." << std::endl;
-			
 		}
 		console_m.unlock();
 	}
 };
+
+std::vector<std::shared_ptr<Metric>> v_m;
